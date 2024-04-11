266eee8edaf4151a7339853234f10fbc6c3b9d58
Wenchang Gao <Wenchang.Gao@tufts.edu>
2024-04-11 12:55:58 -0400

    init

--- example_solution/enums/direction.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Direction(Enum):
-    NONE = 0,
-    NORTH = 1,
-    SOUTH = 2,
-    EAST = 3,
-    WEST = 4
--- example_solution/read_json.py
+++
@@ -1,6 +0,0 @@
-import json 
-
-with open('after.json', 'r') as file:
-    data = json.loads(file)
-# js = json.load('after.json')
-print(data)
